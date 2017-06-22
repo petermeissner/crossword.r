8b551c6413f226a01d5bf021f2410af4d3efd68f
petermeissner <retep.meissner@gmail.com>
2017-06-22 06:09:08 +0200

    adding DEV file

--- dev.R
+++ dev.R
@@ -1,39 +1,33 @@
 library(crossword)
 library(dplyr)
 library(stringr)
+library(googlesheets)
 
 
-# words <-
-#   read.csv(
-#     file     = "../../wortschatz/deu_wikipedia_2016_10K-words.txt",
-#     sep      = "\t",
-#     encoding = "UTF-8"
-#   ) %>%
-#   `[[`(2) %>%
-#   grep("^\\w+$", ., value = TRUE) %>%
-#   unique() %>%
-#   grep("^[[:upper:]]", ., value = TRUE)
+# get words
+googlesheets::gs_auth()
+googlesheets::gs_ls()
+
+gs_crossword <- gs_title("kreuzworträtsel")
+lc_crossword <- gs_read_csv(gs_crossword)[, c("Wort", "Hinweis")]
+
+lc_crossword$Wort <- toupper(lc_crossword$Wort)
+lc_crossword$Wort <- paste0("_", lc_crossword$Wort, "_")
+lc_crossword$Wort <- str_replace_all(lc_crossword$Wort, " +", "")
+lc_crossword$Wort <- str_replace_all(lc_crossword$Wort, "Ä", "AE")
+lc_crossword$Wort <- str_replace_all(lc_crossword$Wort, "Ö", "OE")
+lc_crossword$Wort <- str_replace_all(lc_crossword$Wort, "Ü", "UE")
+lc_crossword$Wort <- str_replace_all(lc_crossword$Wort, "ß", "SS")
+
+lc_crossword <- lc_crossword[sample(seq_len(nrow(lc_crossword))),]
+
+grd <- cw_grid$new(30, 30)
+grd$add_words(lc_crossword$Wort[1:3], lc_crossword$Hinweis[1:3])
+grd$words
 
 
-grd <- cw_grid$new(10, 10)
-
-grd$update_grid_data()
-grd
-
-grd$add_words(c("ganz", "kerze", "ganter"), rep("",3))
-
-grd$add_word("ganz")
-grd$add_word("kerze")
-grd$add_word("ganter")
-
-
-grd$add_word("dingsbums1")
-grd$add_word("dings")
-
-grd$add_word("ddubs")
-grd$add_word("ddubs")
-grd$add_word("ddubs")
-
-
+df <- as.data.frame(grd$letters)
+gs_add_row(gs_crossword, ws=2)
+gs_edit_cells(ss = gs_crossword, ws = 2, input = as.data.frame(grd$letters), trim = TRUE)
 
 
